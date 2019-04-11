d9fc2bbe194cdbf2732250f87a92de21ee8f2be4
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2019-04-11 02:07:13 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Binding
+//
+//  Created by Emmanuel Idehen on 4/8/19.
+//  Copyright © 2019 Emmanuel Idehen. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+//Map example!
+using namespace std;
+int main()
+{
+    
+    map <int, int> map1;
+    map <string, int> map2;
+    
+    //insert elements into the map!
+    
+    map1.insert(pair<int, int>(2,4));
+    map2.insert(pair<string, int>("emmanuel", 28));
+    //using a range based for loop, output the elements in map1 and map2
+    for(auto x : map1)
+    {
+        std::cout << x.first << x.second << "\n";
+    }
+    for(auto x : map2)
+    {
+        std::cout << x.first <<" "<< x.second << "\n";
+    }
+    
+    return 0;
+}
