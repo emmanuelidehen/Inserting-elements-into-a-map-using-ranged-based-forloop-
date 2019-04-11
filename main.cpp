3da91b3fe62f34415aa3014fdfaa596c0f07beb3
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2019-04-11 03:02:11 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Binding
+//  Map c++
 //
 //  Created by Emmanuel Idehen on 4/8/19.
 //  Copyright © 2019 Emmanuel Idehen. All rights reserved.
@@ -18,7 +18,7 @@ int main()
     map <string, int> map2;
     
     //insert elements into the map!
-    
+    int y = 10;
     map1.insert(pair<int, int>(2,4));
     map1.insert(pair<int, int>(6,8));
     map1.insert(pair<int, int>(10,12));
@@ -26,14 +26,55 @@ int main()
     map2.insert(pair<string, int>("idehen", 323));
     map2.insert(pair<string, int>("osagumwenro", 1538));
     //using a range based for loop, output the elements in map1 and map2
+    std::cout<< "before erase was used";//all content of map1
     for(auto x : map1)
     {
-        std::cout << x.first <<" "<< x.second << "\n";
+        
+        std::cout <<"\n"<<x.first <<" "<< x.second << "\n";
     }
+   
+    map1.erase(y);
+    cout<<"after erased was used!";//after erase was used to delete a particular key work the pair was takinh out also
+    for(auto x : map1)
+    {
+        std::cout <<"\n"<<x.first <<" "<< x.second << "\n";
+    }
+  
+     cout<<"Map2!";
+     cout <<"\n";
+    //Map 2 starts
+    
     for(auto x : map2)
     {
-        std::cout << x.first <<" "<< x.second << "\n";
+      
+        std::cout <<"\n" <<x.first <<" "<< x.second << "\n";
     }
-    
+    cout <<"\n";
+   
+ 
     return 0;
 }
+/*
+ test case:
+ 
+ before erase was used
+ 2 4
+ 
+ 6 8
+ 
+ 10 12
+ after erased was used!
+ 2 4
+ 
+ 6 8
+ Map2!
+ 
+ emmanuel 347
+ 
+ idehen 323
+ 
+ osagumwenro 1538
+ 
+ Program ended with exit code: 0
+ 
+ */
